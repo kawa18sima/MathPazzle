d88ab305ef8c39aca00d02aec0cd8e9523fc8f23
Masaya Nakashima <kawa18sima@gmail.com>
2018-03-03 11:39:09 +0900

    add q1.rb
    Q1 anser code add

---
+++ ruby/chapter1/q1.rb
@@ -0,0 +1,17 @@
+# Q1
+num = 11
+
+while true do
+    if num.to_s == num.to_s.reverse &&
+       num.to_s(8) == num.to_s(8).reverse &&
+       num.to_s(2) == num.to_s(2).reverse
+        
+        puts num
+        break
+        
+            
+    end
+        
+    num += 2 
+end
+
